186628f2513fee4414c2e2dc10e2656466fe4d0b
Lushi Li <lushili06@gmail.com>
2013-06-05 12:04:51 -0400

    add functions getFields, trim, and sortData to data prototype

--- js/Protodex.Data.js
+++ js/Protodex.Data.js
@@ -23,18 +23,24 @@ Protodex.Data.prototype = {
 	 * \return	array of field names
 	 */
 	getFields: function()
-	{
-		for (var i=0, l=data.length; i<l; i++) {
-			// write me!
-		}
+	{   var fields = []
+        for(var f in this.data[0]) fields.push(f);
+        return fields;
 	},
 
 	/*!\brief	trims the data to just the specified fields
-	 * \param	fields		array of fields to keep
+	 * \param	fieldsToKeep		array of fields to keep
 	 */
-	trim: function(fields)
-	{
-		// write me!
+	trim: function(fieldsToKeep)
+	{   var fields = this.getFields();
+        var fieldsToDelete = fields.filter(function (f) {   //diff btwn all fields and fields to keep are fields to delete
+                return fieldsToKeep.indexOf(f) == -1;
+            });
+        for(var i=0, l=this.data.length; i<l; i++) {
+            for(var j=0, m=fieldsToDelete.length; j<m; j++)  {
+                delete this.data[i][fieldsToDelete[j]];
+            }
+        }
 	},
 
 	/*!\brief	case-insensitive, in-place sorting of data
@@ -43,9 +49,30 @@ Protodex.Data.prototype = {
 	 *					'ASC': ascending (default)
 	 *					'DSC': descending
 	 */
-	sort: function(sort, dir)
-	{
-		// write me!
-		// NOTE: see JS Array.sort() documentation
+	sortData: function(sort, dir)               //method name changed to sortData to avoid collision with sort
+	{   var order = dir === 'DSC' ? -1 : 1;    //flip order if dir is DSC
+        var by = function (sort) {
+            return function (o, p) {
+                var a, b;
+                if (typeof o === 'object' && typeof p === 'object' && o && p) {
+                    a = o[sort];
+                    b = p[sort];
+                    if (a === b) {
+                        return 0;
+                    }
+                    if (typeof a === typeof b) {
+                        return (a < b ? -1 : 1) * order;
+                    }
+                    return (typeof a < typeof b ? -1 : 1) * order;
+                } else {
+                    throw {
+                        name: 'Error',
+                        message: 'Expected an object when sorting by ' + sort
+                    };
+                }
+            };
+        };
+
+        return this.data.sort(by(sort));
 	}
 };
